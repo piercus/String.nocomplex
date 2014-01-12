8270f17644683d98687f3d9c99df1ab258360f31
cyril agosta <cyril.agosta@gmail.com>
2014-01-12 15:34:53 +0100

    Releasing v0.0.9

--- dist/build/main.js
+++ dist/build/main.js
@@ -8,7 +8,7 @@
 // Copyright (c) Alexandru Marasteanu <alexaholic [at) gmail (dot] com>
 
 /**
- * String.nocomplex version: "0.0.8" Copyright (c) 2011-2012, Cyril Agosta ( cyril.agosta.dev@gmail.com) All Rights Reserved.
+ * String.nocomplex version: "0.0.9" Copyright (c) 2011-2012, Cyril Agosta ( cyril.agosta.dev@gmail.com) All Rights Reserved.
  * Available via the MIT license.
  * see: http://github.com/cagosta/String.nocomplex for details
  */
